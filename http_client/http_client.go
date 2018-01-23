87337157590160ceb87de716cd5cadbc571d0dfd
Amit Kumar Singh <aksinghdce@gmail.com>
2018-01-22 18:44:11 -0800

    one machine working with 4 servers and local grep

--- http_client/http_client.go
+++ http_client/http_client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 )
@@ -20,14 +21,45 @@ func main() {
 	v.Set("ask", "grep")
 	v.Add("search", "tanuki")
 	v.Add("file", "machine1.log")
-	resp, err := http.PostForm("http://grepservice:8080/", v)
-	if err != nil {
-		fmt.Println("There was an error")
-	}
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err == nil {
-		fmt.Println("response:", string(body))
-	}
+	v1 := url.Values{}
+	v1.Set("ask", "grep")
+	v1.Add("search", "0.149471")
+	v1.Add("file", "machine1.log")
+
+	v2 := url.Values{}
+	v2.Set("ask", "grep")
+	v2.Add("search", "1.113414")
+	v2.Add("file", "machine1.log")
+
+	v3 := url.Values{}
+	v3.Set("ask", "grep")
+	v3.Add("search", "3161")
+	v3.Add("file", "machine1.log")
+
+	c := remoteGrep("grepservice", v)
+	c1 := remoteGrep("grepservice1", v1)
+	c2 := remoteGrep("grepservice2", v2)
+	c3 := remoteGrep("grepservice3", v3)
+	fmt.Println("Response from server:", <-c)
+	fmt.Println("Response from server:", <-c1)
+	fmt.Println("Response from server:", <-c2)
+	fmt.Println("Response from server:", <-c3)
+}
+
+func remoteGrep(machine string, cmd url.Values) <-chan string {
+	c := make(chan string)
+	go func() {
+		resp, err := http.PostForm("http://"+machine+":8080/", cmd)
+		if err != nil {
+			log.Fatal("ERROR: sending request to remote http server")
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal("Error reading response from remote")
+		}
+		c <- string(body)
+	}()
+	return c
 }
