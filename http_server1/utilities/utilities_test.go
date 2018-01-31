b7294be6a6e9e3a77b86baa996d1035c674b9acc
Amit Kumar Singh <aksinghdce@gmail.com>
2018-01-31 10:06:11 -0800

    structured the test case for utilities.LocalGrep

--- http_server1/utilities/utilities_test.go
+++ http_server1/utilities/utilities_test.go
@@ -5,9 +5,21 @@ import (
 )
 
 func TestLocalGrep(t *testing.T) {
-	var output string
-	output = LocalGrep([]string{"grep", "-c", "8080", "/go/src/app/Dockerfile"})
-	if output != "1\n" {
-		t.Fatalf(`Sprintf("%%s", empty("7")) = %q want %q`, output, "7")
+	tt := []struct{
+		commandstringsslice []string
+		output string
+	}{
+		{[]string{"grep", "-c", "8080", "/go/src/app/Dockerfile"}, "1"},
 	}
+
+	for _, tc := range tt {
+		var output string
+		output = LocalGrep(tc.commandstringsslice)
+		outputexpected := tc.output
+		outputexpected += "\n"
+		if output != outputexpected {
+			t.Fatalf(`Sprintf("%%s", empty("7")) = %q want %q`, output, "7")
+		}
+	}
+	
 }
