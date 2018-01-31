662b3eb406267635ec82fc4b6d8c217410b990d8
Amit Kumar Singh <aksinghdce@gmail.com>
2018-01-31 09:43:11 -0800

    fixed the test case for LocalGrep

--- http_server1/utilities/utilities_test.go
+++ http_server1/utilities/utilities_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestLocalGrep(t *testing.T) {
 	var output string
-	output = LocalGrep("grep", "-c", "tanuki", "/go/src/app/machine1.log")
-	if output != "7\n" {
+	output = LocalGrep([]string{"grep", "-c", "8080", "/go/src/app/Dockerfile"})
+	if output != "1\n" {
 		t.Fatalf(`Sprintf("%%s", empty("7")) = %q want %q`, output, "7")
 	}
 }
