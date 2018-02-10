e3bbb874324ae023b2af75a3c4714ee777545413
Amit Kumar Singh <aksinghdce@gmail.com>
2018-02-10 15:24:41 -0800

    udp heartbeater and heartbeatserver initial code

--- dgrepservice/multicastheartbeater/multicastheartbeater.go
+++ dgrepservice/multicastheartbeater/multicastheartbeater.go
@@ -1,5 +1,40 @@
-package multicastheartbeater
+package main
 
-func multicastlistener() {
-	
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	ServerAddr, err := net.ResolveUDPAddr("udp", "224.0.0.1:10001")
+	CheckError(err)
+
+	LocalAddr, err := net.ResolveUDPAddr("udp", ":10002")
+	CheckError(err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	CheckError(err)
+
+	defer Conn.Close()
+	i := 0
+	for {
+		msg := strconv.Itoa(i)
+		i++
+		buf := []byte(msg)
+		_, err := Conn.Write(buf)
+		if err != nil {
+			fmt.Println(msg, err)
+		}
+		time.Sleep(time.Second * 1)
+	}
 }
---
+++ dgrepservice/multicastheartbeatserver/multicastheartbeatserver.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println("Exiting with Error: ", err)
+		os.Exit(1)
+	}
+}
+
+/*
+Multicast address: 172.20.0.1 ?
+*/
+func main() {
+	ifsArr, err := net.Interfaces()
+	CheckError(err)
+
+	for _, ifs := range ifsArr {
+		flag := ifs.Flags.String()
+		if strings.Contains(flag, "multicast") {
+			fmt.Println("Interface:", ifs.Name)
+			fmt.Println("Interface Flag:", ifs.Flags.String())
+
+			multicastaddresses, err := ifs.MulticastAddrs()
+			if err != nil {
+				fmt.Println("Fault 1: ", err)
+				continue
+			}
+			for index, addr := range multicastaddresses {
+				fmt.Println("Network:", addr.Network())
+				addstr := addr.String()
+				addstr += ":10001"
+				udpaddr, err := net.ResolveUDPAddr("udp", addstr)
+				if err != nil {
+					fmt.Println("Fault 2: ", err)
+					continue
+				}
+
+				fmt.Printf("multicast address %d : %s\n", index, addr.String())
+				//func ListenMulticastUDP(network string, ifi *Interface, gaddr *UDPAddr) (*UDPConn, error)
+				interf, err := net.InterfaceByIndex(ifs.Index)
+				if err != nil {
+					fmt.Println("Fault 3: ", err)
+					continue
+				}
+				conn, err := net.ListenMulticastUDP("udp", interf, udpaddr)
+				if err != nil {
+					fmt.Println("Fault 4: ", err)
+					continue
+				}
+				defer conn.Close()
+
+				buf := make([]byte, 256)
+				_, _, err2 := conn.ReadFromUDP(buf)
+				if err2 != nil {
+					fmt.Println("Fault 5: ", err2)
+					continue
+				}
+				fmt.Println("Data received:", string(buf))
+			}
+		}
+
+	}
+
+}
