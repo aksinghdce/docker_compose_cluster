0120c4a52a35e921f1114bb0126d1b8be106e940
Amit Kumar Singh <aksinghdce@gmail.com>
2018-03-14 08:48:45 -0700

    stress test passed

--- distributedservices/membership/fsm/fsm_test.go
+++ distributedservices/membership/fsm/fsm_test.go
@@ -1,11 +1,90 @@
 package fsm
 
-import "testing"
+import (
+    "testing"
+    "net"
+    "app/membership/utilities"
+    "context"
+    "math/rand"
+	"fmt"
+    "time"
+    "app/membership/communication"
+)
+
+var udpServerTests = []struct {
+	snet, saddr string // server endpoint
+	tnet, taddr string // target endpoint for client
+}{
+	{snet: "udp", saddr: "0.0.0.0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: ":0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "0.0.0.0:0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::ffff:0.0.0.0]:0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::]:0", tnet: "udp", taddr: "::1"},
+
+	{snet: "udp", saddr: ":0", tnet: "udp", taddr: "::1"},
+	{snet: "udp", saddr: "0.0.0.0:0", tnet: "udp", taddr: "::1"},
+	{snet: "udp", saddr: "[::ffff:0.0.0.0]:0", tnet: "udp", taddr: "::1"},
+	{snet: "udp", saddr: "[::]:0", tnet: "udp", taddr: "127.0.0.1"},
+
+	{snet: "udp", saddr: ":0", tnet: "udp4", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "0.0.0.0:0", tnet: "udp4", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::ffff:0.0.0.0]:0", tnet: "udp4", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::]:0", tnet: "udp6", taddr: "::1"},
+
+	{snet: "udp", saddr: ":0", tnet: "udp6", taddr: "::1"},
+	{snet: "udp", saddr: "0.0.0.0:0", tnet: "udp6", taddr: "::1"},
+	{snet: "udp", saddr: "[::ffff:0.0.0.0]:0", tnet: "udp6", taddr: "::1"},
+	{snet: "udp", saddr: "[::]:0", tnet: "udp4", taddr: "127.0.0.1"},
+
+	{snet: "udp", saddr: "127.0.0.1:0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::ffff:127.0.0.1]:0", tnet: "udp", taddr: "127.0.0.1"},
+	{snet: "udp", saddr: "[::1]:0", tnet: "udp", taddr: "::1"},
+
+	{snet: "udp4", saddr: ":0", tnet: "udp4", taddr: "127.0.0.1"},
+	{snet: "udp4", saddr: "0.0.0.0:0", tnet: "udp4", taddr: "127.0.0.1"},
+	{snet: "udp4", saddr: "[::ffff:0.0.0.0]:0", tnet: "udp4", taddr: "127.0.0.1"},
+
+	{snet: "udp4", saddr: "127.0.0.1:0", tnet: "udp4", taddr: "127.0.0.1"},
+
+	{snet: "udp6", saddr: ":0", tnet: "udp6", taddr: "::1"},
+	{snet: "udp6", saddr: "[::]:0", tnet: "udp6", taddr: "::1"},
+	{snet: "udp6", saddr: "[::1]:0", tnet: "udp6", taddr: "::1"},
+}
+
 
 func TestFsm(t *testing.T) {
     fsm1 := Init(1)
     fsm2 := Init(2)
     fsm1.ProcessFsm()
     fsm2.ProcessFsm()
+
+    //Run communication test in parallel to do some stress testing
+    go func() {
+        ctx := context.Background()
+	listenChannel, speakChannel := communication.Comm(ctx, 50000, 50000)
+	for caseNumber, tt := range udpServerTests {
+		fmt.Printf("Running Test Number:%d\n", caseNumber)
+		packet := utilities.Packet{
+			FromIp: net.ParseIP(tt.taddr),
+			ToIp: net.ParseIP(tt.saddr),
+			Seq: rand.Int63(),
+		}
+		var receivedPacket utilities.Packet
+		for i := 0; i<5; i++ {
+			//Sending test packet
+			speakChannel <- packet
+			timeout := time.After(10 * time.Second)
+			select {
+				//Receiving test packet
+			case receivedPacket = <-listenChannel:
+				fmt.Printf("Packet:%v received\n",receivedPacket)
+			case <-timeout:
+				t.Fatal("Packet not received in 10 second")
+			}
+		}
+    }
+    }()
+    
+    
     t.Log("Well done!")
 }
